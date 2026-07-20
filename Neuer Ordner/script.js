3e118fa12632c8c944d17e46f10585b5df82c026
Strudelgame <yuri@rhoelzl.it>
2026-07-20 18:26:57 +0200

    Delete Neuer Ordner directory

--- Neuer Ordner/script.js
+++
@@ -1,6 +0,0 @@
-const editor = document.getElementById('editor');
-const preview = document.getElementById('preview');
-
-editor.addEventListener('input', () => {
-    preview.innerHTML = marked.parse(editor.value);
-});
